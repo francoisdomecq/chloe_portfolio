ca2c6270cf19cd56a37491bbc73a530265b95cf2
francoisdmq <francois.domecq@ratp.fr>
2024-10-11 13:10:37 +0200

    fix home page

--- chloe_portfolio/src/domains/home/pages/home/home.tsx
+++ chloe_portfolio/src/domains/home/pages/home/home.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import { PortfolioPage } from "../../../core";
 import VideoPlayer from "../../../works/pages/project-details/components/video-player/video-player";
 
@@ -7,24 +5,14 @@ import "./home.scss";
 
 
 const Home = ()=>{
-    const [isVideoPlaying,setIsVideoPlaying] = useState(true);
     return (
-        <>
-            {isVideoPlaying &&
-              <div className="home__showreel">
-                  <VideoPlayer source="./projects/GAILLARD_CHLOE_SHOWREEL_24.mp4" loop={false} onChangeIsPlaying={setIsVideoPlaying}/>
-              </div>
-            }
-            {!isVideoPlaying &&
-              <PortfolioPage >
-                  <div className="home-page">
-                      <div className="home__showreel-small">
-                          <VideoPlayer source="./projects/GAILLARD_CHLOE_SHOWREEL_24.mp4" loop={false}/>
-                      </div>
-                  </div>
-              </PortfolioPage>
-            }
-        </>
+        <PortfolioPage >
+            <div className="home-page">
+                <div className="home__showreel-small">
+                    <VideoPlayer source="./projects/GAILLARD_CHLOE_SHOWREEL_24.mp4" loop={false}/>
+                </div>
+            </div>
+        </PortfolioPage>
 
     );
 };
