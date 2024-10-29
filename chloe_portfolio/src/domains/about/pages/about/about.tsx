a6d478a547e4750e70cc33fb5853000fde55e6aa
francoisdmq <francois.domecq@ratp.fr>
2024-10-29 17:09:13 +0100

    refacto about page

--- chloe_portfolio/src/domains/about/pages/about/about.tsx
+++ chloe_portfolio/src/domains/about/pages/about/about.tsx
@@ -9,17 +9,19 @@ import Skills from "../../components/skills/skills";
 
 import "./about.scss";
 
+import { useNavigate } from "react-router-dom";
+
 const About = () => {
     const { t }=useTranslation("about");
+    const navigate = useNavigate();
 
     const routerHistory = [{ route:"/",label:"Home" }, { route: "/about", label: "About" }];
 
     return (
         <PortfolioPage>
             <div className="about">
-                <NavigationHistory history={routerHistory}/>
+                <NavigationHistory className="about__navigation-history" history={routerHistory}/>
                 <div className="about__intro-container">
-                    <h1 className="about__title">{t("title")}</h1>
                     <div className="about__introduction">
                         <p>{t("introduction")}</p>
                     </div>
@@ -27,6 +29,7 @@ const About = () => {
                 <Skills/>
                 <Schools/>
                 <Experiences/>
+                <div className="about__contact-button" onClick={()=>navigate("/contact")}>{t("contact")}</div>
             </div>
         </PortfolioPage>
     );
